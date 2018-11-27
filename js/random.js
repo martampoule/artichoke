f0a5778499a07e821de9536a374344d129aac395
marta <marta.cattaneo.96@gmail.com>
2018-11-27 21:47:13 +0100

    sistemato grafico che non funzionava

--- js/random.js
+++ js/random.js
@@ -18,41 +18,6 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function() {
-
-    var letters = ["को", "ख", "ग", "घ", "और", "    ", "च", "जी", "ज", "मैं", "एल", "मीटर", "    ", "या", "पी", "क्ष", "आर", "रों", "टी", "में", "एक्स", "और", "से", "    "];
-
-    var count = 0;
-    setInterval(function() {
-
-        if(count < 90) {
-            var rand = letters[Math.floor(Math.random() * letters.length)];
-            $(".randomText1").append(rand);
-        }
-        count++;
-    }, 100);
-
-
-
-})
-
-$(document).ready(function() {
-
-    var letters = ["को", "ख", "ग", "घ", "और", "    ", "च", "जी", "ज", "मैं", "एल", "मीटर", "    ", "या", "पी", "क्ष", "आर", "रों", "टी", "में", "एक्स", "और", "से", "    "];
-
-    var count = 0;
-    setInterval(function() {
-
-        if(count < 140) {
-            var rand = letters[Math.floor(Math.random() * letters.length)];
-            $(".randomText2").append(rand);
-        }
-        count++;
-    }, 200);
-
-
-
-})
 
 
 
