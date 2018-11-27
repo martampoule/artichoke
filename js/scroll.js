293de2acc2fc4e778ab4c8f3bf8df410d4087bee
marta <marta.cattaneo.96@gmail.com>
2018-11-27 19:17:02 +0100

    provato scroll- no

---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+<script>
+$(function() {
+          $.scrollify({
+            section : ".example-classname",
+            $.scrollify({
+sectionName : "section-name",
+interstitialSection : "",
+easing: "easeOutExpo",
+scrollSpeed: 1100,
+offset : 0,
+scrollbars: true,
+standardScrollElements: "",
+setHeights: true,
+overflowScroll: true,
+updateHash: true,
+touchScroll:true,
+before:function() {},
+after:function() {},
+afterResize:function() {},
+afterRender:function() {}
+});
+          });
+        });
+      </script>
