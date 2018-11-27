2f32fbd5773fea724b742f549c6be50f401075ec
marta <marta.cattaneo.96@gmail.com>
2018-11-27 17:58:18 +0100

    ordinato sito

--- js/random.js
+++ js/random.js
@@ -7,7 +7,27 @@ $(document).ready(function() {
 
         if(count < 100) {
             var rand = letters[Math.floor(Math.random() * letters.length)];
-            $("#randomText").append(rand);
+            $(".randomText").append(rand);
+        }
+        count++;
+    }, 300);
+
+
+
+})
+
+
+
+$(document).ready(function() {
+
+    var letters = ["को", "ख", "ग", "घ", "और", "    ", "च", "जी", "ज", "मैं", "एल", "मीटर", "    ", "या", "पी", "क्ष", "आर", "रों", "टी", "में", "एक्स", "और", "से", "    "];
+
+    var count = 0;
+    setInterval(function() {
+
+        if(count < 90) {
+            var rand = letters[Math.floor(Math.random() * letters.length)];
+            $(".randomText1").append(rand);
         }
         count++;
     }, 100);
@@ -15,6 +35,28 @@ $(document).ready(function() {
 
 
 })
+
+$(document).ready(function() {
+
+    var letters = ["को", "ख", "ग", "घ", "और", "    ", "च", "जी", "ज", "मैं", "एल", "मीटर", "    ", "या", "पी", "क्ष", "आर", "रों", "टी", "में", "एक्स", "और", "से", "    "];
+
+    var count = 0;
+    setInterval(function() {
+
+        if(count < 140) {
+            var rand = letters[Math.floor(Math.random() * letters.length)];
+            $(".randomText2").append(rand);
+        }
+        count++;
+    }, 200);
+
+
+
+})
+
+
+
+
 
 
 
