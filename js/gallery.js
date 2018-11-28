35dfc1a6b29b2946dcf0150de2f2902d403aa6f2
marta <marta.cattaneo.96@gmail.com>
2018-11-28 16:42:49 +0100

    commenti carousel semi finiti

--- js/gallery.js
+++ js/gallery.js
@@ -1,48 +1,4 @@
-/* external js: flickity.pkgd.js
 
-var $carousel = $('.carousel').flickity({
-    imagesLoaded: true,
-    percentPosition: false,
-    pageDots: false,
-    prevNextButtons: true,
-    initialIndex: 1,
-    wrapAround: true,
-    freeScroll: true, 
-    
-  });
-
-  
-  var $imgs = $carousel.find('.carousel-cell img');
-  // get transform property
-  var docStyle = document.documentElement.style;
-  var transformProp = typeof docStyle.transform == 'string' ?
-    'transform' : 'WebkitTransform';
-  // get Flickity instance
-  var flkty = $carousel.data('flickity');
-  
-  $carousel.on( 'scroll.flickity', function() {
-    flkty.slides.forEach( function( slide, i ) {
-      var img = $imgs[i];
-      var x = ( slide.target + flkty.x ) * -1/400;
-      img.style[ transformProp ] = 'translateX(' + x + 'px)';
-    });
-  });
-  
-  $carousel.on( 'staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
-    // dismiss if cell was not clicked
-    if ( !cellElement ) {
-      return;
-    }
-    // change cell background with .is-clicked
-    $carousel.find('.is-clicked').removeClass('is-clicked');
-    $( cellElement ).addClass('is-clicked');
-  });
-  
-  
-  
-*/
-
-// external js: flickity.pkgd.js
 
 var $carouselMain = $('.carousel-main').flickity({
   pageDots: false,
@@ -60,5 +16,24 @@ $carouselNav.on( 'staticClick.flickity', function(  event, pointer, cellElement,
   }
   
 })
+
+var $carouselMain1 = $('.carousel-main1').flickity({
+  pageDots: false,
+});
+
+var $carouselNav1 = $('.carousel-nav1').flickity({
+  contain: true,
+  pageDots: false,
+  prevNextButtons: false
+});
+
+$carouselNav1.on( 'staticClick.flickity', function(  event, pointer, cellElement, cellIndex ) {
+  if ( cellElement ) {
+    $carouselMain1.flickity( 'select', cellIndex );
+  }
+  
+})
+
+
   
   
